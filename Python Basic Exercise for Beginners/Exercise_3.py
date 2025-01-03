d86ca074e2a3d76cbfc1aec6c9f7b73271af0ac1
AnkitaSaxena8765693852 <ankitasaxena.workidea@gmail.com>
2025-01-03 23:52:44 +0000

    Completed Exercise

---
+++ Python Basic Exercise for Beginners/Exercise_3.py
@@ -0,0 +1,13 @@
+# Exercise 3: Print characters present at an even index number
+
+"""Write a Python code to accept a string from the user and display characters present at an even 
+    index number.
+
+For example, str = "PYnative". so your code should display 'P', 'n', 't', 'v'."""
+
+original = input("Original String is : ")
+
+print("Printing only even Index chars")
+for i in range (len(original)):
+    if (i % 2 == 0):
+        print(original[i])
