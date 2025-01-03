549a5af827f9387fcb67895fc23743447a8e8c21
AnkitaSaxena8765693852 <ankitasaxena.workidea@gmail.com>
2025-01-03 23:34:29 +0000

    Completed Questions

--- Python Basic Exercise for Beginners/Exercise_1.py
+++ Python Basic Exercise for Beginners/Exercise_1.py
@@ -3,10 +3,18 @@
 """ Given two integer numbers, write a Python code to return their product only if the product is
  equal to or lower than 1000. Otherwise, return their sum."""
 
+# input first number from user
 num1 = int(input("number1 = "))
+
+# input second number from user
 num2 = int(input("number2 = "))
 
-if num1 * num2 < 1000:
-    print("The result is ", (num1 * num2))
-else:
-    print("The result is ",(num1 + num2))
+# checking the logic by using functions
+
+def logic(num1, num2):
+    if num1 * num2 < 1000:  
+        print("The result is ", (num1 * num2))
+    else:
+        print("The result is ",(num1 + num2))
+
+logic(num1, num2)
--- Python Basic Exercise for Beginners/Exercise_2.py
+++ Python Basic Exercise for Beginners/Exercise_2.py
@@ -1,1 +1,14 @@
 # Exercise 2: Print the Sum of a Current Number and a Previous number
+
+"""Write a Python code to iterate the first 10 numbers, and in each iteration, print the sum of 
+the current and previous number"""
+
+# Taking input from the user
+num = int(input("Printing current and previous number sum in a range: "))
+
+# declare a counter variable
+prev = 0
+for i in range (num):
+    sum = prev + i
+    print("Current Number ",i,"Previous Number", prev,"Sum:", sum)
+    prev = i
