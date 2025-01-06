688dd39b8649e015fa1bb8f3803366012d815b26
AnkitaSaxena8765693852 <ankitasaxena.workidea@gmail.com>
2025-01-06 23:21:34 +0000

    Completed Questions

--- Python Basic Exercise for Beginners/Exercise_9.py
+++ Python Basic Exercise for Beginners/Exercise_9.py
@@ -3,17 +3,18 @@
 """Write a Python code to check if the given number is palindrome. 
     A palindrome number is a number that is the same after reverse. For example, 545 is the palindrome number"""
 
-original_num = int(input("Enter the number: "))
-reverse_num = 0
+num = int(input("Enter the number: "))
+original_num = num
+rev = 0
 
 
 while num != 0:
     n = num % 10
     rev = rev * 10 + n
     num = num // 10
-print(rev)
-print(num)
-if ( rev == num):
+
+
+if ( rev == original_num):
     print("given number is palindrome number")
 else:
     print("given number is not palindrome number")
