9c65471d8f48857379ff438215a443e1ed7d5a49
AnkitaSaxena8765693852 <ankitasaxena.workidea@gmail.com>
2025-01-07 14:31:56 +0000

    Completed Questions

--- Python Basic Exercise for Beginners/Exercise_12.py
+++ Python Basic Exercise for Beginners/Exercise_12.py
@@ -8,3 +8,17 @@
 | Next $10,000         | 10          |
 | The remaining        | 20          |
 """
+
+taxable_income = int(input("Enter the taxable income: "))
+n1 = 0
+
+if ( taxable_income <= 10000):
+    tax = taxable_income * 0
+elif ( taxable_income <= 20000):
+    tax = (taxable_income - 10000) * 0.10
+else:
+    n1 = 10000 * 0.10
+    n2 = (taxable_income - 20000) * 0.20
+    tax = n1 + n2
+
+print(tax)
