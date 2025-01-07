26694ca5d28e875d1ed553a59420f3519c59f918
AnkitaSaxena8765693852 <ankitasaxena.workidea@gmail.com>
2025-01-07 17:49:08 +0000

    Completed Questions

---
+++ Python Input and Output Exercise/Exercise_01.py
@@ -0,0 +1,8 @@
+# Exercise 1: Accept numbers from a user
+
+"""Write a program to accept two numbers from the user and calculate multiplication"""
+
+num1 = int(input("Enter the first number = "))
+num2 = int(input("Enter the second number = "))
+
+print("The multiplication of two numbers = ", (num1 * num2))
