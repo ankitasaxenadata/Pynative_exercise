fe375f5caa69b503df5ab9b7632bc8fcbdb8445b
AnkitaSaxena8765693852 <ankitasaxena.workidea@gmail.com>
2025-01-09 22:07:22 +0000

    Completed Questions

--- Python Input and Output Exercise/Exercise_06.py
+++ Python Input and Output Exercise/Exercise_06.py
@@ -1,2 +1,8 @@
 # Exercise 6: Write all content of a given file into a new file by skipping line number 5
 
+count = 0
+with open('test.txt') as f:
+    for line in f:
+        count = count + 1 # count the line number so that we can eliminate line number 5
+        if( count != 5):
+            print(line.strip())
