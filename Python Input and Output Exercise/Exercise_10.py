e310097e637910848174a3c2daeb1f88d1457d2b
AnkitaSaxena8765693852 <ankitasaxena.workidea@gmail.com>
2025-01-09 23:04:41 +0000

    Completed Questions

---
+++ Python Input and Output Exercise/Exercise_10.py
@@ -0,0 +1,10 @@
+# Exercise 10: Read line number 4 from the following file
+
+f = open("test.txt")
+
+word = f.readlines()
+count = 0
+for i in range(len(word)):
+    count = count + 1
+    if (count == 4):
+        print(word[i])
