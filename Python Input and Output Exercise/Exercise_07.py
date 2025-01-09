26068f20ecdcc935849de7312f149517f8ad3d72
AnkitaSaxena8765693852 <ankitasaxena.workidea@gmail.com>
2025-01-09 22:13:16 +0000

    Completed Questions

---
+++ Python Input and Output Exercise/Exercise_07.py
@@ -0,0 +1,7 @@
+# Exercise 7: Accept any three string from one input() call
+
+name_1, name_2, name_3 = input("Enter three string = ").split()
+
+print("Name1 : ", name_1)
+print("Name2 : ", name_2)
+print("Name3 : ", name_3)
